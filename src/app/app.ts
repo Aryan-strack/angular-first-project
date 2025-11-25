ae104616ddb9aac2bc955d9a488cab6f78917d0f
Aryan-strack <aryanahmad478@gmail.com>
2025-11-25 22:48:42 +0500

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-app');
